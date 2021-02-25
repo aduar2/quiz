b616a2609f004e1ba1121473d9cc4cf69967c33d
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-02-24 23:54:57 -0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,57 +2,46 @@ $(document).ready(function() {
 	$(".a").hide();
 	$(".d1").click(function() {
 		$("#a1").toggle();
-		$("h1").css({"color":"red", "background-color": "yellow"})
 	});
 	
 	$(".d2").click(function() {
 		$("#a2").toggle();
-		$("h1").css({"color":"orange", "background-color": "yellow"})
 	});
 	
 	$(".d3").click(function() {
 		$("#a3").toggle();
-		$("h1").css({"color":"yellow", "background-color": "yellow"})
 	});
 	
 	$(".d4").click(function() {
 		$("#a4").toggle();
-		$("h1").css({"color":"green", "background-color": "yellow"})
 	});
 	
 	$(".d5").click(function() {
 		$("#a5").toggle();
-		$("h1").css({"color":"blue", "background-color": "yellow"})
 	});
 	
 	$(".d6").click(function() {
 		$("#a6").toggle();
-		$("h1").css({"color":"purple", "background-color": "yellow"})
 	});
 	
 	$(".d7").click(function() {
 		$("#a7").toggle();
-		$("h1").css({"color":"pink", "background-color": "yellow"})
 	});
 	
 	$(".d8").click(function() {
 		$("#a8").toggle();
-		$("h1").css({"color":"lightblue", "background-color": "yellow"})
 	});
 	
 	$(".d9").click(function() {
 		$("#a9").toggle();
-		$("h1").css({"color":"lightblue", "background-color": "yellow"})
 	});
 	
 	$(".d10").click(function() {
 		$("#a10").toggle();
-		$("h1").css({"color":"lightblue", "background-color": "yellow"})
 	});
 	
 	$(".d11").click(function() {
 		$("#a11").toggle();
-		$("h1").css({"color":"lightblue", "background-color": "yellow"})
 	});
 });
 
